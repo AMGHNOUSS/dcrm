df648d8edd9841df6267e7e3c939846532249e78
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-11-11 21:48:20 +0100

    Login users

--- website/views.py
+++ website/views.py
@@ -1,4 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
+from django.contrib import messages
 
 def home(request):
-    return render(request, 'home.html')
+    dict = {}
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user:
+            login(request, user)
+            messages.success(request, "You have been Logged In!")
+            return redirect('home')
+        else:
+            messages.success(request, "There are was an error Try again...!")
+            return redirect('home')
+    else:
+        return render(request, 'home.html', dict)
+
+def login_user(request):
+    pass
+
+def logout_user(request):
+    pass
