eb00d705b06e0bc17358c16ec86a9c08117c59d9
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-11-11 17:40:39 +0100

    Build Out The basic app

--- website/urls.py
+++ website/urls.py
@@ -1,7 +1,7 @@
 """Defines URL patterns for learning_logs."""
 from django.urls import path
 from . import views
-app_name = 'website'
+
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.home, name="home"),
 ]
--- website/views.py
+++ website/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
 
 def home(request):
-    """The home page for Learning Log."""
     return render(request, 'home.html')
