28f0da5d12f72982d68ef60b08ff8cefe394f16d
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-11-12 15:33:23 +0100

    Register Users

--- website/views.py
+++ website/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
-
+from .forms import SignUpForm
 def home(request):
     dict = {}
     if request.method == "POST":
@@ -18,8 +18,24 @@ def home(request):
     else:
         return render(request, 'home.html', dict)
 
-def login_user(request):
-    pass
-
 def logout_user(request):
-    pass
+    logout(request)
+    messages.success(request, "You have been Logged out..")
+    return redirect('home')
+
+def register_user(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # Authenticate and login
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, "You Have Successfully Registered! Welcome!")
+            return redirect('home')
+    else:
+        form = SignUpForm()
+        return render(request, 'register.html', {'form':form})
+    return render(request, 'register.html', {'form':form})
