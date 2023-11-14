60fbb6e9aa0c6c620d2ba85bc6ee791fe9bf1f30
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-11-14 23:48:23 +0100

    Update record

--- website/views.py
+++ website/views.py
@@ -72,3 +72,16 @@ def add_record(request):
 	else:
 		messages.success(request, "You Must Be Logged In...")
 		return redirect('home')
+
+def update_record(request, pk):
+    if request.user.is_authenticated:
+        current_record = Record.objects.get(id=pk)
+        form = AddRecordForm(request.POST or None, instance=current_record)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Record Has Been Updated")
+            return redirect('home')
+        return render(request, 'update_record.html', {'form':form})
+    else:
+        messages.success("You must Logged In!")
+        return redirect('home')
